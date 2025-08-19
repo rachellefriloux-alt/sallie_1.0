3a03bac70eda81031c0a44babb43a29733ccee0a
rachellefriloux-alt <rachellefriloux@gmail.com>
2025-08-19 14:07:51 -0500

    01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -167,5 +167,3 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
-
-tasks.withType<Test> { useJUnitPlatform() }
